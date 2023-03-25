1633f15e210298a01ef077fc01bb0fc97d98c78f
mariolarios <108195141+mariolarios@users.noreply.github.com>
2023-03-24 20:27:10 -0400

    Created basic routing for signing up, logging in and logging out a user.

--- src/app.js
+++ src/app.js
@@ -1,20 +1,45 @@
-const express = require('express');
-const app = express();
-const cors = require('cors')
-const favicon = require('express-favicon');
-const logger = require('morgan');
+require("dotenv").config({ path: "src/.env" });
+require("express-async-errors");
 
-const mainRouter = require('./routes/mainRouter.js');
+const express = require("express");
+const app = express();
+
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const favicon = require("express-favicon");
+const logger = require("morgan");
+const connectDB = require("./db/connect");
+
+///express json
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
 
 // middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(logger('dev'));
-app.use(express.static('public'))
-app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger("dev"));
+app.use(express.static("public"));
+app.use(favicon(__dirname + "/public/favicon.ico"));
+
+app.get("/", (req, res) => {
+  res.send("stayfinder");
+});
 
 // routes
-app.use('/api/v1', mainRouter);
+const authRouter = require("./routes/auth");
 
-module.exports = app;
+app.use("/api/v1/auth", authRouter);
+
+/// set up port
+const port = process.env.PORT || 8000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ src/models/User.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minlength: 3,
+    maxLength: 30,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Please provide email"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide valid email",
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 6,
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { userId: this._id, email: this.email, time: Date.now() },
+    `${process.env.JWT_SECRET}`,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  );
+};
+
+UserSchema.methods.comparePassword = async function (userPassword) {
+  const isMatch = await bcrypt.compare(userPassword, this.password);
+  return isMatch;
+};
+
+module.exports = mongoose.model("User", UserSchema);
