6be739be5590b4608f89b10484479485ac360a82
Miguel <74852114+madebymiguel@users.noreply.github>
2023-04-07 15:19:28 -0700

    Add try catch to most auth controllers. Remove createJWT function and user user model createJWT function.  Add getUserToken which checks for cookie.

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,16 +1,9 @@
 const jwt = require("jsonwebtoken");
 
-const createJWT = ({ payload }) => {
-  const token = jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_LIFETIME,
-  });
-  return token;
-};
-
 const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET);
 
 const attachCookiesToResponse = ({ res, user }) => {
-  const token = createJWT({ payload: user });
+  const token = user.createJWT();
 
   const oneDay = 1000 * 60 * 60 * 24;
 
@@ -23,7 +16,6 @@ const attachCookiesToResponse = ({ res, user }) => {
 };
 
 module.exports = {
-  createJWT,
   isTokenValid,
   attachCookiesToResponse,
 };
