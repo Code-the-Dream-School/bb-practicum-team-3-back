7780ffd51c5f8966368fdc0309f6e90cb88af81d
Miguel <74852114+madebymiguel@users.noreply.github>
2023-04-12 17:07:29 -0700

    Refactor booking calls. Add room number updated hotels object to include more data, updated some error handling

--- src/api/bookingCalls.js
+++ src/api/bookingCalls.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const isIncluded = require("../utils/isIncluded");
 
 const searchLocation = async (destination) => {
   const options = {
@@ -15,31 +16,18 @@ const searchLocation = async (destination) => {
     const response = await axios.request(options);
     return response.data[0].dest_id;
   } catch (error) {
-    return error;
+    throw new Error("Unable to fetch location");
   }
 };
 
 const searchHotels = async (
-  locationId,
+  destinationName,
+  destinationId,
+  guestNumber,
+  roomNumber,
   checkinDate,
-  checkoutDate,
-  guestNumber
+  checkoutDate
 ) => {
-  const currentDate = new Date();
-  const nextDate = new Date(currentDate);
-
-  nextDate.setDate(currentDate.getDate() + 1);
-  const formattedCurrentDate = currentDate.toISOString().slice(0, 10);
-  const formattedNextDate = nextDate.toISOString().slice(0, 10);
-
-  if (checkinDate === "") {
-    checkinDate = formattedCurrentDate;
-  }
-
-  if (checkoutDate === "") {
-    checkoutDate = formattedNextDate;
-  }
-
   const options = {
     method: "GET",
     url: "https://booking-com.p.rapidapi.com/v1/hotels/search",
@@ -50,10 +38,10 @@ const searchHotels = async (
       checkout_date: `${checkoutDate}`,
       adults_number: `${guestNumber}`,
       order_by: "popularity",
-      dest_id: `${locationId}`,
+      dest_id: `${destinationId}`,
       filter_by_currency: "USD",
       locale: "en-gb",
-      room_number: "1",
+      room_number: `${roomNumber}`,
     },
     headers: {
       "X-RapidAPI-Key": `${process.env.RapidAPI_Key}`,
@@ -65,25 +53,31 @@ const searchHotels = async (
     const response = await axios.request(options);
 
     const filterResults = response.data.result.map((item) => ({
-      hotel_id: item?.hotel_id,
-      hotel_name: item?.hotel_name,
-      photo_url: item?.max_photo_url,
-      district: item?.district,
-      city: item?.city,
-      review_score: item?.review_score,
-      review_score_word: item?.review_score_word,
-      review_number: item?.review_nr,
-      distance_to_cc: item?.distance_to_cc_formatted,
-      gross_amount: item?.composite_price_breakdown?.gross_amount,
-      is_free_cancellable: item?.is_free_cancellable,
-      hotel_include_breakfast: item?.hotel_include_breakfast,
-      has_swimming_pool: item?.has_swimming_pool,
-      has_free_parking: item?.has_free_parking,
+      id: item.hotel_id,
+      name: item.hotel_name,
+      image_url: item.max_photo_url,
+      district: item.district,
+      city: item.city,
+      review_score: item.review_score,
+      review_score_word: item.review_score_word,
+      review_number: item.review_nr,
+      distance: item.distance_to_cc_formatted,
+      price: Math.ceil(item.composite_price_breakdown?.gross_amount.value),
+      is_free_cancellable: isIncluded(item.is_free_cancellable),
+      hotel_include_breakfast: isIncluded(item.hotel_include_breakfast),
+      has_swimming_pool: isIncluded(item.has_swimming_pool),
+      has_free_parking: isIncluded(item.has_free_parking),
     }));
 
-    return filterResults;
+    const data = {
+      destinationName: destinationName,
+      count: response.data.count,
+      results: filterResults,
+    };
+
+    return data;
   } catch (error) {
-    return error;
+    throw new Error("Unable to fetch hotels");
   }
 };
 
